fff56663c0b41207411fd2ae35be78e19da708b7
Catherine Lin <mscatherinelin@gmail.com>
2017-03-09 19:05:01 -0800

    created packet header file

---
+++ packet.h
@@ -0,0 +1,8 @@
+#define PACKET_SIZE 1024
+
+struct packet {
+  int type; //0: SYN, 1:DATA, 2: ACK, 3: FIN
+  int seq; //Sequence number of packet
+  int size; //Data size
+  char data[PACKET_SIZE];
+};
